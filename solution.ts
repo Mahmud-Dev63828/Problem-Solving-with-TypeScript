218e90f23aa9f3955aa1b5ea2dbee65630f33455
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-11-17 23:48:02 +0600

    problem 6 completed

--- solution.ts
+++ solution.ts
@@ -68,3 +68,24 @@ const users = [
   { id: 2, name: "Asha", email: "asha@example.com", isActive: false },
   { id: 3, name: "Rumi", email: "rumi@example.com", isActive: true },
 ];
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+const printBookDetails = (book: Book): string => {
+  const availability = book.isAvailable ? "Yes" : "No";
+  return `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`;
+};
+
+const myBook: Book = {
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+printBookDetails(myBook);
