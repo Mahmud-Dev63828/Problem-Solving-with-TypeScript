d88701960dfc0fa1405fa6c6bb1dda57b713aaf4
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-11-17 22:54:33 +0600

    problem 3 solved

--- solution.ts
+++ solution.ts
@@ -20,3 +20,19 @@ const getLength = (input: GetLengthType): number => {
   }
   return 0;
 };
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  }
+}
+
+const person1 = new Person("John Doe", 30);
+const person2 = new Person("Alice", 25);
