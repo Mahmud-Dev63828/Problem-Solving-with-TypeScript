e616c1681bc682fb56ee44716782cdebb615bca1
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-11-17 23:26:48 +0600

    problem 4 and 5 solved

--- solution.ts
+++ solution.ts
@@ -30,9 +30,41 @@ class Person {
     this.age = age;
   }
   getDetails(): string {
-    return `Name: ${this.name}, Age: ${this.age}`;
+    return `'Name: ${this.name}, Age: ${this.age}'`;
   }
 }
 
 const person1 = new Person("John Doe", 30);
 const person2 = new Person("Alice", 25);
+
+type Item = {
+  title: string;
+  rating: number;
+};
+
+const filterByRating = (items: Item[]): Item[] => {
+  return items.filter((item) => item.rating >= 4);
+};
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+const filterActiveUsers = (users: User[]): User[] => {
+  return users.filter((user) => user.isActive);
+};
+
+const users = [
+  { id: 1, name: "Rakib", email: "rakib@example.com", isActive: true },
+  { id: 2, name: "Asha", email: "asha@example.com", isActive: false },
+  { id: 3, name: "Rumi", email: "rumi@example.com", isActive: true },
+];
